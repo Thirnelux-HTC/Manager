08361956b96f196e30a4bbc358265dec9a2db2ea
ThibaultHTC <Thibault-htc_2@outlook.fr>
2020-02-25 13:01:04 +0100

    v 1.0.0

---
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnCalc= (Button) findViewById(R.id.btnCalc);
+        Button btnCont= (Button) findViewById(R.id.btnContacts);
+        btnCont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pacgeCont= new Intent(MainActivity.this,contacts.class);
+                startActivity(pacgeCont);
+            }
+        });
+
+        btnCalc.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pageCalc= new Intent( MainActivity.this,calculatrice.class);
+                startActivity(pageCalc);
+            }
+        }));
+    }
+
+}
